d9eff10e1cf32477771d6f4a73375dd3b5de3573
ssaprt <130990888+ssaprt@users.noreply.github.com>
2026-07-26 17:28:37 +0400

    edit general page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { EasyPagination } from "@/content/react/UI-Components/Pagination/EasyPagination";
+
 export default function Home() {
-    return <div>sobaka</div>;
+    return (
+        <div>
+            <EasyPagination />
+        </div>
+    );
 }
