3f7fc5218761be9c8b28c323be78bb0cf34a6187
ssaprt <130990888+ssaprt@users.noreply.github.com>
2026-07-28 15:18:58 +0400

    upd

--- src/layouts/primary/Main/Main.tsx
+++ src/layouts/primary/Main/Main.tsx
@@ -1,9 +1,18 @@
+import { useAppContextValues } from "@/context/appContext";
+import clsx from "clsx";
+import React from "react";
 import { Scroll } from "../Scroll";
 import styles from "./Main.module.css";
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
+    const { menu } = useAppContextValues();
+    const pending = menu.pending;
+
     return (
-        <main className={styles.main} id="main">
+        <main
+            className={clsx(styles.main, pending && styles.pending)}
+            id="main"
+        >
             <Scroll />
             {children}
         </main>
