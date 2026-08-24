e412f10b83a96f990f7c2390e35b5b2016090bfe
ssaprt <130990888+ssaprt@users.noreply.github.com>
2026-08-24 17:07:44 +0400

    added css classes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,5 @@
 import { cssRoute } from "./css-route/css.route";
 import { openDataRoute } from "./open-data-route/open-data.route";
-
 import { reactRoute } from "./react-route/react.route";
 import { referencesRoute } from "./references-route/references.route";
 
